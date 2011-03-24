f29a6b65582e777cb19a898747a1e25527ce9680
Francesco Canessa <makevoid@gmail.com>
2011-03-24 07:21:31 +0100

    manual testing

--- models/thorz.rb
+++ models/thorz.rb
@@ -14,21 +14,24 @@ class Thorz
   
   def search
     url = URI.parse URL % @query
+    res = nil
     timeout(4) do 
       res = Net::HTTP.get_response(url)
     end
     #puts res.body
+    
+    if res
+      doc = Nokogiri::HTML(res.body)
+      doc.css("#searchResult tr").each do |tr|
+        name = tr.css(".detName").inner_text
+        next if name.nil? || name == ""
+        magnet = tr.css("a").map{ |a| a if a["title"] =~ /magnet/i }.compact
+        magnet = magnet.first["href"] unless magnet.nil?
+        seeds = tr.css("td")[2]
+        seeds = seeds.inner_text unless seeds.nil?
 
-    doc = Nokogiri::HTML(res.body)
-    doc.css("#searchResult tr").each do |tr|
-      name = tr.css(".detName").inner_text
-      next if name.nil? || name == ""
-      magnet = tr.css("a").map{ |a| a if a["title"] =~ /magnet/i }.compact
-      magnet = magnet.first["href"] unless magnet.nil?
-      seeds = tr.css("td")[2]
-      seeds = seeds.inner_text unless seeds.nil?
-
-      @results << {name: name, magnet: magnet, seeds: seeds} 
+        @results << {name: name, magnet: magnet, seeds: seeds} 
+      end
     end
     #puts @results 
     true
