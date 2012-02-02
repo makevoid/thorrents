cf7dde00b0bb95e3c67663d34dc1a2af3542f74c
makevoid <makevoid@gmail.com>
2012-02-02 18:00:18 +0100

    tpb.se

--- models/thorz.rb
+++ models/thorz.rb
@@ -3,41 +3,41 @@ require 'cgi'
 require 'nokogiri'
 
 class Thorz
-  URL = "http://thepiratebay.org/search/%s/0/7/0"
-  
+  URL = "http://thepiratebay.se/search/%s/0/7/0"
+
   attr_reader :results
-  
+
   def initialize(query)
     @query = CGI.escape query
     @results = []
   end
-  
-  def proxied_search(host) 
+
+  def proxied_search(host)
     return false if @query.blank?
     url = URI.parse "http://#{host}/search/#{@query}.json"
     res = nil
-    timeout(5) do 
+    timeout(5) do
       res = Net::HTTP.get_response url
     end
-    if res  
+    if res
       JSON.parse(res.body)["results"].each do |rs|
         @results << {name: rs["name"], magnet: rs["magnet"], seeds: rs["seeds"]}
       end
     end
   end
-  
+
   def search
     url = URI.parse URL % @query
     res, reason, errror = nil
     begin
-      timeout(5) do 
+      timeout(5) do
         res = Net::HTTP.get_response url
       end
     rescue Timeout::Error => e
       reason = "Timeout", error = e
     end
     #puts res.body
-    
+
     if res
       doc = Nokogiri::HTML(res.body)
       doc.css("#searchResult tr").each do |tr|
@@ -48,10 +48,10 @@ class Thorz
         seeds = tr.css("td")[2]
         seeds = seeds.inner_text unless seeds.nil?
 
-        @results << {name: name, magnet: magnet, seeds: seeds} 
+        @results << {name: name, magnet: magnet, seeds: seeds}
       end
     end
-    #puts @results 
+    #puts @results
     true
   end
 end
