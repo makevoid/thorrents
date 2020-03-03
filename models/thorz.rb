88301f0516f600014013742577091e8cc1a0b26c
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2020-03-03 19:15:20 +0000

    use se.net by default as a host, refactor the model/api a bit

--- models/thorz.rb
+++ models/thorz.rb
@@ -2,15 +2,12 @@ require 'net/http'
 require 'cgi'
 require 'nokogiri'
 
-
-
 class Thorz
   # HOST = "thepiratebay.se"
   # HOST = "piratebay.org"
   # HOST = "pirateproxy.in"
-  HOST = "" # USE ONE OF THE ABOVE HOST here
-
-  URL = "http://#{HOST}/search/%s/0/7/0"
+  HOST = "thepiratebay.se.net" # use any host here, this is just for sample
+  URL = "https://#{HOST}/search/%s/0/7/0"
 
   TIMEOUT = 5
 
@@ -23,7 +20,7 @@ class Thorz
 
   def proxied_search(host)
     return false if @query.blank?
-    url = URI.parse "http://#{host}/search/#{@query}.json"
+    url = URI.parse "https://#{host}/search/#{@query}.json"
     res = nil
     timeout(TIMEOUT) do
       res = Net::HTTP.get_response url
@@ -45,24 +42,27 @@ class Thorz
     rescue Timeout::Error => e
       reason = "Timeout", error = e
     end
-    #puts res.body
 
     if res
       doc = Nokogiri::HTML(res.body)
-      doc.css("#searchResult tr").each do |tr|
+      # just use "#main-content > table#searchResult tr" if you want
+      # to use another HOST
+      mod = ""
+      mod = "[2]" if HOST == "thepiratebay.se.net" # suppress ads on se.net
+      doc.css("#main-content > table#searchResult#{mod} tr").each do |tr|
         name = tr.css(".detName").inner_text
         next if name.nil? || name == ""
-        magnet = tr.css("a").map{ |a| a if a["title"] =~ /magnet/i }.compact
-        magnet = magnet.first["href"] unless magnet.nil?
+        magnet = tr.css("a").map{ |a| a if a["title"] =~ /download this torrent using magnet/i }.compact
+        magnet = magnet.first && magnet.first["href"] unless magnet.nil?
         seeds = tr.css("td")[2]
         seeds = seeds.inner_text unless seeds.nil?
 
         @results << {name: name, magnet: magnet, seeds: seeds}
       end
     else
-      @results << { name: "an error occcurred, maybe TPB server is down", magnet: "#", seeds: 0}
+      @results << { name: "an error occcurred, maybe TPB server is down", magnet: "#", seeds: 0 }
     end
-    #puts @results
+
     true
   end
 
@@ -70,9 +70,7 @@ class Thorz
 
   def get(uri)
      resp = Net::HTTP.get_response uri
-     if resp.code == "301"
-       resp = Net::HTTP.get_response URI.parse(resp.header['location'])
-     end
+     resp = Net::HTTP.get_response URI.parse(resp.header['location']) if resp.code == "301"
      resp
   end
 end
